c62df0675272b907b07a217c56e42b151bd021a7
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-06-08 22:30:50 +0530

    timepicker done

--- src/TimePicker.jsx
+++ src/TimePicker.jsx
@@ -8,6 +8,15 @@ const TimeData = [
   "05:00 AM",
   "06:00 AM",
   "07:00 AM",
+  "08:00 AM",
+  "09:00 AM",
+  "11:00 AM",
+  "12:00 AM",
+  "01:00 PM",
+  "02:00 PM",
+  "03:00 PM",
+  "04:00 PM",
+  "05:00 PM",
 ];
 
 const TimeComponent = ({ handleTime, date, time }) => {
@@ -58,6 +67,8 @@ const TimePicker = ({ date, setTimeActive }) => {
 
   return (
     <div className="timepicker">
+      <h2>Select Time</h2>
+
       <div className="timepicker-wrapper">
         {TimeData.map((time) => (
           <TimeComponent handleTime={handleTime} date={date} time={time} />

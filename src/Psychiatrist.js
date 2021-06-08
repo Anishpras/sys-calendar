c1b3c5579f295be0d45e1c0b4d3524891551511f
kumard3 <kuamrdeepanshu157200@gmail.com>
2021-06-08 11:41:45 +0530

    half-responsive

--- src/Psychiatrist.js
+++ src/Psychiatrist.js
@@ -1,13 +1,36 @@
-import React from 'react'
-import './Psychiatrist.css'
+import React from "react";
+import "./Psychiatrist.css";
+import data from "./data/data";
 import profile from "./assest/profile.png";
+
 function Psychiatrist() {
-    return (
-        <div  className="psychiatrist">
-        <img  src={profile} />
-            <h1>psychiatrist</h1>
-        </div>
-    )
+  return (
+    <div className="psychiatrist">
+      <div className="psy-wrapper">
+        {data.map((data) => {
+          return (
+            <div className="psy-content">
+              <div className="psy-header">
+                <img className="psy-profile" src={profile} />
+                <div className="psy-header-right">
+                  <h3>{data.name} </h3>
+                  <p> {data.email} </p>
+                </div>
+              </div>
+              <div className="psy-body">
+                <h1> About </h1>
+                <h4>{data.about} </h4>
+                <h1> Education </h1>
+                <h4>{data.education} </h4>
+                <h1> Work Experience </h1>
+                <h4>{data.work} </h4>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Psychiatrist  
+export default Psychiatrist;

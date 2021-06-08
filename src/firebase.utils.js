e455bcc3c4647fdd66ea8117ce3bf6bbe48d679b
Mansi Sharma <mansi.isha.mansi@gmail.com>
2021-06-08 14:43:20 +0530

    changed firebase config

--- src/firebase.utils.js
+++ src/firebase.utils.js
@@ -4,26 +4,24 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 const prodConfig = {
-  apiKey: 'AIzaSyCYCCbz5J6KGlPHQcfZ6V55AUeaq-l3Jn8',
-  authDomain: 'raahee-5a80f.firebaseapp.com',
-  databaseURL: 'https://raahee-5a80f.firebaseio.com',
-  projectId: 'raahee-5a80f',
-  storageBucket: 'raahee-5a80f.appspot.com',
-  messagingSenderId: '385300765927',
-  appId: '1:385300765927:web:7163f7a7f5059251521642',
-  measurementId: 'G-P4BTGSXBEK',
+    apiKey: "AIzaSyAA_UAzrbj8R3j7I_oqgve-YyjaVrCDKpM",
+    authDomain: "raahee-web-calendar-e9d76.firebaseapp.com",
+    projectId: "raahee-web-calendar-e9d76",
+    storageBucket: "raahee-web-calendar-e9d76.appspot.com",
+    messagingSenderId: "1536936436",
+    appId: "1:1536936436:web:b7dc40e24245eb7bffd1a1",
+    measurementId: "G-B2KHFP7SZZ",
 };
 
 // DEV
 const devConfig = {
-  apiKey: 'AIzaSyDbZtFhcgwc1PFvwIX569C_Ei6-LEsJ2zs',
-  authDomain: 'raahee-testing.firebaseapp.com',
-  databaseURL: 'https://raahee-testing.firebaseio.com',
-  projectId: 'raahee-testing',
-  storageBucket: 'raahee-testing.appspot.com',
-  messagingSenderId: '410583406163',
-  appId: '1:410583406163:web:eb5759cd29c1da605658a6',
-  measurementId: 'G-LR7RF5S9BZ',
+    apiKey: "AIzaSyAA_UAzrbj8R3j7I_oqgve-YyjaVrCDKpM",
+    authDomain: "raahee-web-calendar-e9d76.firebaseapp.com",
+    projectId: "raahee-web-calendar-e9d76",
+    storageBucket: "raahee-web-calendar-e9d76.appspot.com",
+    messagingSenderId: "1536936436",
+    appId: "1:1536936436:web:b7dc40e24245eb7bffd1a1",
+    measurementId: "G-B2KHFP7SZZ",
 };
 
 console.log(process.env.REACT_APP_PROD);
